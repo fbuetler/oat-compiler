e365294ea620a4ece69a806662c563ad129ad737
Florian Buetler <flbuetle@ethz.ch>
2019-10-11 22:14:05 +0200

    add and shift tests

--- hw2/studenttests.ml
+++ hw2/studenttests.ml
@@ -47,8 +47,8 @@ let make_instr_test_print (label:string) (instructions:ins list) (check: mach ->
   in (label, wrapped_test)
 
 let make_bin_op_test (op:opcode) (a:int64) (b:int64) (res:int64) (flags:flags) =
-  let label = Printf.sprintf "%s (%s - %s) = %s"
-      (opcode_to_string op) (Int64.to_string a) (Int64.to_string b) (Int64.to_string res) in
+  let label = Printf.sprintf "%s %s %s = %s"
+      (opcode_to_string op) (Int64.to_string b) (Int64.to_string a) (Int64.to_string res) in
   let get_res (m:mach) = int64_of_sbytes (sbyte_list m.mem (mem_size-8)) in
   make_instr_test_print label
     [(Movq, [Imm (Lit a); ~%Rax])
@@ -58,11 +58,16 @@ let make_bin_op_test (op:opcode) (a:int64) (b:int64) (res:int64) (flags:flags) =
     (fun m -> Printf.sprintf "res = %s, flags = {fo=%B, fs=%B, fz=%B}"
         (Int64.to_string @@ get_res m) m.flags.fo m.flags.fs m.flags.fz)
 
-let student_instruction_tests = [
-  make_instr_test "best test ever" 
-    [(Movq, [~$42; ~%Rax]); (Movq, [~%Rax; stack_offset (-8L)])] 
-    (fun m -> int64_of_sbytes (sbyte_list m.mem (mem_size-16)) = 42L)
-  ;
+let student_instruction_tests_flo = [
+  make_bin_op_test Addq 42424242L 123123L 42547365L {fo=false; fs=false; fz=false}; (* random add *)
+  make_bin_op_test Addq 9223372036854775807L 1L 9223372036854775808L {fo=true; fs=true; fz=false}; (* overflow add: T_Max + 1 = |T_Min| *)
+  make_bin_op_test Sarq 4L 1L 2L {fo=false; fs=false; fz=false}; (* small random arithmetic shift *)
+  make_bin_op_test Sarq 1588476L 13L 193L {fo=false; fs=false; fz=false}; (* big random arithmetic shift *)
+  make_bin_op_test Sarq (-1L) 10L (-1L) {fo=false; fs=true; fz=false}; (* arithmetic shifting only ones shouldnt change anything*)
+  make_bin_op_test Sarq 0L 3L 0L {fo=false; fs=false; fz=true}; (* arithmetic shifting 0 shouldnt change anything *)
+  make_bin_op_test Sarq 8L 3L 1L {fo=false; fs=false; fz=false}; (* arithmetic sfhit: MSB is 1 *)
+  make_bin_op_test Shrq 8L 4L 0L {fo=false; fs=false; fz=false}; (* logical shift *)
+  make_bin_op_test Shrq 8L 4L 0L {fo=false; fs=false; fz=false}; (* logical shift *)
 ]
 
 (* TODO merge this with the other tests *)
@@ -77,7 +82,7 @@ let student_instruction_tests_philippe = [
 ]
 
 let provided_tests : suite = [
-  Test ("student_instruction_tests", student_instruction_tests);
+  Test ("student_instruction_tests_flo", student_instruction_tests_flo);
   Test ("student_instruction_tests_philippe", student_instruction_tests_philippe);
   Test ("Student-Provided Big Test for Part III: Score recorded as PartIIITestCase", []);
 ] 
