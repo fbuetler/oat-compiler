d6da1c5cefb045803d0d929b354601327dd44e68
Philippe Voinov <philippevoinov@gmail.com>
2019-10-11 19:56:40 +0200

    Framework for own tests

--- hw2/studenttests.ml
+++ hw2/studenttests.ml
@@ -1,15 +1,23 @@
 open Assert
 open X86
 open Simulator
+open Gradedtests
+open Asm
 
-(* These tests are provided by you -- they will be graded manually *)
+let make_instr_test (label:string) (instructions:ins list) (check: mach -> bool) =
+  let pad_instr (instr:ins) = [InsB0 (instr);InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag] in
+  let m = test_machine @@ List.flatten @@ List.map pad_instr instructions in
+  let t = machine_test "<see check function>" (List.length instructions) m check in
+  (label, t)
 
-(* You should also add additional test cases here to help you   *)
-(* debug your program.                                          *)
-
+let student_instruction_tests = [
+  make_instr_test "best test ever" 
+    [(Movq, [~$42; ~%Rax]); (Movq, [~%Rax; stack_offset (-8L)])] 
+    (fun m -> int64_of_sbytes (sbyte_list m.mem (mem_size-16)) = 42L)
+  ;
+]
 
 let provided_tests : suite = [
-  Test ("Student-Provided Big Test for Part III: Score recorded as PartIIITestCase", [
-  ]);
-
+  Test ("student_instruction_tests", student_instruction_tests);
+  Test ("Student-Provided Big Test for Part III: Score recorded as PartIIITestCase", []);
 ] 
