4004448473c2b062937751b0b42806539ffacfcf
Philippe Voinov <philippevoinov@gmail.com>
2019-10-11 21:54:28 +0200

    Test Incq

--- hw2/studenttests.ml
+++ hw2/studenttests.ml
@@ -58,6 +58,18 @@ let make_bin_op_test (op:opcode) (a:int64) (b:int64) (res:int64) (flags:flags) =
     (fun m -> Printf.sprintf "res = %s, flags = {fo=%B, fs=%B, fz=%B}"
         (Int64.to_string @@ get_res m) m.flags.fo m.flags.fs m.flags.fz)
 
+let make_un_op_test (op:opcode) (a:int64) (res:int64) (flags:flags) =
+  let label = Printf.sprintf "%s %s = %s"
+      (opcode_to_string op) (Int64.to_string a) (Int64.to_string res) in
+  let get_res (m:mach) = int64_of_sbytes (sbyte_list m.mem (mem_size-8)) in
+  make_instr_test_print label
+    [(Movq, [Imm (Lit a); ~%Rax])
+    ;(op, [~%Rax])
+    ;(Movq, [~%Rax; stack_offset 0L])] 
+    (fun m -> get_res m = res && m.flags = flags)
+    (fun m -> Printf.sprintf "res = %s, flags = {fo=%B, fs=%B, fz=%B}"
+        (Int64.to_string @@ get_res m) m.flags.fo m.flags.fs m.flags.fz)
+
 let student_instruction_tests = [
   make_instr_test "best test ever" 
     [(Movq, [~$42; ~%Rax]); (Movq, [~%Rax; stack_offset (-8L)])] 
@@ -73,7 +85,12 @@ let student_instruction_tests_philippe = [
   make_bin_op_test Subq (-7L) (-7L) 0L {fo=false; fs=false; fz=true};
   make_bin_op_test Subq Int64.min_int 1L Int64.max_int {fo=true; fs=false; fz=false};
   make_bin_op_test Subq 0L Int64.max_int (Int64.neg Int64.max_int) {fo=false; fs=true; fz=false};
-  make_bin_op_test Subq 0L Int64.min_int Int64.min_int {fo=true; fs=true; fz=false}
+  make_bin_op_test Subq 0L Int64.min_int Int64.min_int {fo=true; fs=true; fz=false};
+  make_un_op_test Incq 0L 1L {fo=false; fs=false; fz=false};
+  make_un_op_test Incq 5L 6L {fo=false; fs=false; fz=false};
+  make_un_op_test Incq (-1L) 0L {fo=false; fs=false; fz=true};
+  make_un_op_test Incq (-2L) (-1L) {fo=false; fs=true; fz=false};
+  make_un_op_test Incq Int64.max_int Int64.min_int {fo=true; fs=true; fz=false};
 ]
 
 let provided_tests : suite = [
