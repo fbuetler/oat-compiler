4f33b2e10be6597a92b9969253bf93d22bd1f507
Philippe Voinov <philippevoinov@gmail.com>
2019-11-11 08:33:20 +0100

    Fix bitwise or

--- hw4/studenttests.ml
+++ hw4/studenttests.ml
@@ -14,7 +14,8 @@ let own_tests = [
   ("atprograms/own/call_arg_order.oat", "", "3");
   ("atprograms/own/array_exp_order.oat", "", "3");
   ("atprograms/own/if_exp_once.oat", "", "2");
-  ("atprograms/own/not_call.oat", "", "2");
+  ("atprograms/own/not_call.oat", "", "0");
+  ("atprograms/own/bitwise.oat", "", "6");
   (* TODO add all the tests from atprogrmas/own *)
 ]
 
