6015219fde7cc74bcf00236ce89b4ddf1c6d93ae
Philippe Voinov <philippevoinov@gmail.com>
2019-12-07 17:17:45 +0100

    Constprop starting to work

--- hw6/constprop.ml
+++ hw6/constprop.ml
@@ -39,13 +39,14 @@ type fact = SymConst.t UidM.t
 let insn_flow (u,i:uid * insn) (d:fact) : fact =
   let get_op (op: Ll.operand) : SymConst.t = begin match op with
     | Id uid -> UidM.find uid d
+    | Const v -> SymConst.Const v
     | _ -> SymConst.NonConst
   end in
   let stuff (a: Ll.operand) (b: Ll.operand) (f: int64 -> int64 -> int64) : SymConst.t = 
     begin match (get_op a, get_op b) with
       | (SymConst.Const va, SymConst.Const vb) -> SymConst.Const (f va vb)
+      | (SymConst.NonConst, _) | (_, SymConst.NonConst) -> SymConst.NonConst
       | (SymConst.UndefConst, _) | (_, SymConst.UndefConst) -> SymConst.UndefConst
-      | (SymConst.NonConst, _) | (_, SymConst.NonConst) -> SymConst.UndefConst
     end
   in
   let out_sym: SymConst.t = begin match i with
@@ -102,13 +103,16 @@ struct
      flow into a node. You may find the UidM.merge function useful 
   *)
   let combine (ds:fact list) : fact = 
-    List.fold_left (UidM.union (fun _ a b -> begin match (a, b, a == b) with
-        | (SymConst.Const _, SymConst.Const _, true) -> Some a
-        | (SymConst.Const _, SymConst.Const _, false) -> Some SymConst.NonConst
-        (* TODO should NonConst or UndefConst come first? *)
-        | (SymConst.NonConst, _, _) | (_, SymConst.NonConst, _) -> Some SymConst.NonConst
-        | (SymConst.UndefConst, _, _) | (_, SymConst.UndefConst, _) -> Some SymConst.UndefConst
-      end)) UidM.empty ds
+    (* Printf.printf "combine %d\n" @@ List.length ds; *)
+    List.fold_left (UidM.union (fun _ a b ->
+        (* Printf.printf "combine (%s, %s)\n" (SymConst.to_string a) (SymConst.to_string b); *)
+        begin match (a, b, a == b) with
+          | (SymConst.Const _, SymConst.Const _, true) -> Some a
+          | (SymConst.Const _, SymConst.Const _, false) -> Some SymConst.NonConst
+          (* TODO should NonConst or UndefConst come first? *)
+          | (SymConst.NonConst, _, _) | (_, SymConst.NonConst, _) -> Some SymConst.NonConst
+          | (SymConst.UndefConst, _, _) | (_, SymConst.UndefConst, _) -> Some SymConst.UndefConst
+        end)) UidM.empty ds
 end
 
 (* instantiate the general framework ---------------------------------------- *)
@@ -137,10 +141,30 @@ let analyze (g:Cfg.t) : Graph.t =
 let run (cg:Graph.t) (cfg:Cfg.t) : Cfg.t =
   let open SymConst in
 
+  let map_operands (i: Ll.insn) (f: Ll.operand -> Ll.operand) : Ll.insn = begin match i with
+    | Binop (op, ty, a, b) -> Binop (op, ty, f a, f b)
+    | Alloca ty -> Alloca ty
+    | Load (ty, op) -> Load (ty, f op)
+    | Store (ty, src, dest) -> Store (ty, f src, f dest)
+    | Icmp (ty, op, a, b) -> Icmp (ty, op, f a, f b)
+    | Call (ret, func, args) -> Call (ret, func, List.map (fun (ty, op) -> (ty, f op)) args)
+    | Bitcast (in_ty, op, out_ty) -> Bitcast (in_ty, f op, out_ty)
+    | Gep (ty, op, l) -> Gep (ty, f op, l)
+  end in
+
   let cp_block (l:Ll.lbl) (cfg:Cfg.t) : Cfg.t =
     let b = Cfg.block cfg l in
     let cb = Graph.uid_out cg l in
-    failwith "Constprop.cp_block unimplemented"
+    let new_insns = List.map (fun (uid, instr) -> 
+        let get_new_op op = begin match op with
+          | Id op_uid -> begin match UidM.find_opt op_uid @@ cb uid with
+              | Some Const v -> Ll.Const v
+              | _ -> op
+            end
+          | _ -> op
+        end in
+        (uid, map_operands instr get_new_op)
+      ) b.insns in
+    Cfg.add_block l { insns = new_insns; term = b.term } cfg
   in
-
   LblS.fold cp_block (Cfg.nodes cfg) cfg
