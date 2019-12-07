15a204c9a2410fca400d142c405ec26967be2927
Philippe Voinov <philippevoinov@gmail.com>
2019-12-07 11:48:28 +0100

    Autoformat

--- hw6/alias.ml
+++ hw6/alias.ml
@@ -5,19 +5,19 @@ open Datastructures
 
 (* The lattice of abstract pointers ----------------------------------------- *)
 module SymPtr =
-  struct
-    type t = MayAlias           (* uid names a pointer that may be aliased *)
-           | Unique             (* uid is the unique name for a pointer *)
-           | UndefAlias         (* uid is not in scope or not a pointer *)
+struct
+  type t = MayAlias           (* uid names a pointer that may be aliased *)
+         | Unique             (* uid is the unique name for a pointer *)
+         | UndefAlias         (* uid is not in scope or not a pointer *)
 
-    let compare : t -> t -> int = Pervasives.compare
+  let compare : t -> t -> int = Pervasives.compare
 
-    let to_string = function
-      | MayAlias -> "MayAlias"
-      | Unique -> "Unique"
-      | UndefAlias -> "UndefAlias"
+  let to_string = function
+    | MayAlias -> "MayAlias"
+    | Unique -> "Unique"
+    | UndefAlias -> "UndefAlias"
 
-  end
+end
 
 (* The analysis computes, at each program point, which UIDs in scope are a unique name
    for a stack slot and which may have aliases *)
@@ -32,7 +32,7 @@ type fact = SymPtr.t UidM.t
      may be aliased
    - Other instructions do not define pointers
 
- *)
+*)
 let insn_flow ((u,i):uid * insn) (d:fact) : fact =
   failwith "Alias.insn_flow unimplemented"
 
@@ -42,35 +42,35 @@ let terminator_flow t (d:fact) : fact = d
 
 (* module for instantiating the generic framework --------------------------- *)
 module Fact =
-  struct
-    type t = fact
-    let forwards = true
+struct
+  type t = fact
+  let forwards = true
 
-    let insn_flow = insn_flow
-    let terminator_flow = terminator_flow
-    
-    (* UndefAlias is logically the same as not having a mapping in the fact. To
-       compare dataflow facts, we first remove all of these *)
-    let normalize : fact -> fact = 
-      UidM.filter (fun _ v -> v != SymPtr.UndefAlias)
+  let insn_flow = insn_flow
+  let terminator_flow = terminator_flow
 
-    let compare (d:fact) (e:fact) : int = 
-      UidM.compare SymPtr.compare (normalize d) (normalize e)
+  (* UndefAlias is logically the same as not having a mapping in the fact. To
+     compare dataflow facts, we first remove all of these *)
+  let normalize : fact -> fact = 
+    UidM.filter (fun _ v -> v != SymPtr.UndefAlias)
 
-    let to_string : fact -> string =
-      UidM.to_string (fun _ v -> SymPtr.to_string v)
+  let compare (d:fact) (e:fact) : int = 
+    UidM.compare SymPtr.compare (normalize d) (normalize e)
 
-    (* TASK: complete the "combine" operation for alias analysis.
+  let to_string : fact -> string =
+    UidM.to_string (fun _ v -> SymPtr.to_string v)
 
-       The alias analysis should take the join over predecessors to compute the
-       flow into a node. You may find the UidM.merge function useful.
+  (* TASK: complete the "combine" operation for alias analysis.
 
-       It may be useful to define a helper function that knows how to take the
-       join of two SymPtr.t facts.
-    *)
-    let combine (ds:fact list) : fact =
-      failwith "Alias.Fact.combine not implemented"
-  end
+     The alias analysis should take the join over predecessors to compute the
+     flow into a node. You may find the UidM.merge function useful.
+
+     It may be useful to define a helper function that knows how to take the
+     join of two SymPtr.t facts.
+  *)
+  let combine (ds:fact list) : fact =
+    failwith "Alias.Fact.combine not implemented"
+end
 
 (* instantiate the general framework ---------------------------------------- *)
 module Graph = Cfg.AsGraph (Fact)
@@ -87,8 +87,8 @@ let analyze (g:Cfg.t) : Graph.t =
   let alias_in = 
     List.fold_right 
       (fun (u,t) -> match t with
-                    | Ptr _ -> UidM.add u SymPtr.MayAlias
-                    | _ -> fun m -> m) 
+         | Ptr _ -> UidM.add u SymPtr.MayAlias
+         | _ -> fun m -> m) 
       g.Cfg.args UidM.empty 
   in
   let fg = Graph.of_cfg init alias_in g in
