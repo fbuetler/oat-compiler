04f64e9d724eb19f4a809cc3e7f3779894012725
Philippe Voinov <philippevoinov@gmail.com>
2019-12-15 15:41:19 +0100

    Make final sum in stopgreedy deterministically generated

--- hw6/gen-stopgreedy.js
+++ hw6/gen-stopgreedy.js
@@ -1,6 +1,5 @@
 const NUM_VARS = 4;
 const NUM_MIX_ADDS = 150;
-const NUM_SUM_ADDS = Math.ceil(NUM_VARS * 1.5);
 
 function times(n, f) {
   return new Array(n).fill(0).map((v, i) => f(i));
@@ -19,7 +18,7 @@ ${times(NUM_VARS, i => `  var x${i} = argc;`).join('\n')}
   for (var i = 0; i < 100000000; i = i + 1;) {
 ${times(NUM_MIX_ADDS, i => `    ${randVar()} = ${randVar()} + ${randVar()};`).join('\n')}
 
-${times(NUM_SUM_ADDS, i => `    sum = sum + ${randVar()} + ${randVar()};`).join('\n')}
+${times(NUM_VARS, i => `    sum = x${i} + sum + x${i};`).join('\n')}
   }
 
   return sum;
