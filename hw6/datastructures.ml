8c1cce4c577e470ea476f8b0251e53b5717bdea4
Philippe Voinov <philippevoinov@gmail.com>
2019-12-15 23:01:54 +0100

    Restore original datastructures.ml

--- hw6/datastructures.ml
+++ hw6/datastructures.ml
@@ -2,108 +2,109 @@
 
 (** Comparable, printable type *)
 module type OrdPrintT =
-sig
-  type t
-  val compare : t -> t -> int
-  val to_string : t -> string
-end
+  sig
+    type t
+    val compare : t -> t -> int
+    val to_string : t -> string
+  end
 
 (** Extended sets *)
 module type SetS =
-sig
-  include Set.S
-  val of_list : elt list -> t     (* added to Set.S in OCaml 4.02 *)
-  val to_string : t -> string
-  val string_of_elt : elt -> string
-  val printer : Format.formatter -> t -> unit
-end
+  sig
+    include Set.S
+    val of_list : elt list -> t     (* added to Set.S in OCaml 4.02 *)
+    val to_string : t -> string
+    val string_of_elt : elt -> string
+    val printer : Format.formatter -> t -> unit
+  end
 
 module MakeSet (Ord : OrdPrintT) : SetS with type elt = Ord.t =
-struct
-  include Set.Make (Ord)
+  struct
+    include Set.Make (Ord)
 
-  let of_list = List.fold_left (fun s e -> add e s) empty
+    let of_list = List.fold_left (fun s e -> add e s) empty
+    
+    let to_string t =
+      let s = elements t
+              |> List.map Ord.to_string
+              |> String.concat ", "
+      in
+      "{" ^ s ^ "}"
 
-  let to_string t =
-    let s = elements t
-            |> List.map Ord.to_string
-            |> String.concat ", "
-    in
-    "{" ^ s ^ "}"
+    let string_of_elt = Ord.to_string
+  
+    let printer f t = Format.pp_print_string f (to_string t)
 
-  let string_of_elt = Ord.to_string
-
-  let printer f t = Format.pp_print_string f (to_string t)
-
-end
+  end
 
 (** Extended maps *)
 module type MapS =
-sig
-  include Map.S
-  val update : ('a -> 'a) -> key -> 'a t -> 'a t
-  val find_or : 'a -> 'a t -> key -> 'a
-  val update_or : 'a -> ('a -> 'a) -> key -> 'a t -> 'a t
-  val diff_keys : ('a -> 'a -> int) -> 'a t -> 'a t -> key list
-  val to_string : (key -> 'a -> string) -> 'a t -> string
-  val printer : (key -> 'a -> string) -> Format.formatter -> 'a t -> unit
-end
+  sig
+    include Map.S
+    val update : ('a -> 'a) -> key -> 'a t -> 'a t
+    val find_or : 'a -> 'a t -> key -> 'a
+    val update_or : 'a -> ('a -> 'a) -> key -> 'a t -> 'a t
+    val diff_keys : ('a -> 'a -> int) -> 'a t -> 'a t -> key list
+    val to_string : (key -> 'a -> string) -> 'a t -> string
+    val printer : (key -> 'a -> string) -> Format.formatter -> 'a t -> unit
+  end
 
 module MakeMap (Ord : OrdPrintT) : MapS with type key = Ord.t =
-struct
-  include Map.Make (Ord)
+  struct
+    include Map.Make (Ord)
 
-  let update f k m =
-    add k (f @@ find k m) m
+    let update f k m =
+      add k (f @@ find k m) m
 
-  let find_or d m k =
-    try find k m with Not_found -> d
+    let find_or d m k =
+      try find k m with Not_found -> d
 
-  let diff_keys cmp_v m n =
-    let module S = MakeSet(Ord) in
-    let has_binding_or_add m k v l =
-      try if cmp_v v @@ find k m == 0 then l else S.add k l
-      with Not_found -> S.add k l
-    in
-    S.empty |> fold (has_binding_or_add n) m
-    |> fold (has_binding_or_add m) n
-    |> S.elements
+    let diff_keys cmp_v m n =
+      let module S = MakeSet(Ord) in
+      let has_binding_or_add m k v l =
+        try if cmp_v v @@ find k m == 0 then l else S.add k l
+        with Not_found -> S.add k l
+      in
+      S.empty |> fold (has_binding_or_add n) m
+              |> fold (has_binding_or_add m) n
+              |> S.elements
 
-  let update_or d f k m = 
-    add k (f @@ find_or d m k) m
+    let update_or d f k m = 
+      add k (f @@ find_or d m k) m
 
-  let to_string val_str t =
-    let s = bindings t
-            |> List.map (fun (k,v) -> Ord.to_string k ^ "=" ^ val_str k v)
-            |> String.concat ", "
-    in
-    "{" ^ s ^ "}"
+    let to_string val_str t =
+      let s = bindings t
+              |> List.map (fun (k,v) -> Ord.to_string k ^ "=" ^ val_str k v)
+              |> String.concat ", "
+      in
+      "{" ^ s ^ "}"
 
-  let printer val_str f t = Format.pp_print_string f (to_string val_str t)
+    let printer val_str f t = Format.pp_print_string f (to_string val_str t)
 
-end
+  end
 
 (** Useful instances *)
 
 module Lbl =
-struct
-  type t = Ll.lbl
-  let compare = String.compare
-  let to_string l = l
-end
+  struct
+    type t = Ll.lbl
+    let compare = String.compare
+    let to_string l = l
+  end
 
 module LblM = MakeMap (Lbl)
 module LblS = MakeSet (Lbl)
 
 module Uid =
-struct
-  type t = Ll.uid
-  let compare = String.compare
-  let to_string u = "%" ^ u
-end
+  struct
+    type t = Ll.uid
+    let compare = String.compare
+    let to_string u = "%" ^ u
+  end
 
 module UidS = MakeSet (Uid)
 module UidM = MakeMap (Uid)
+
 
 (** For testing   *)
 let uidm (b:(Ll.uid * 'a) list) : 'a UidM.t =
