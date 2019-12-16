2159abbd241f9e7c888183c4b4822e3388deb7cf
Florian Buetler <flbuetle@ethz.ch>
2019-12-16 13:32:04 +0100

    run without 01 flag as well

--- hw6/timing-config.js
+++ hw6/timing-config.js
@@ -7,17 +7,36 @@ const TARGET_FILES = [
 const COMPILER_CONFIGURATIONS = [
   {
     name: "baseline",
-    flags: ["-O1", "--liveness", "trivial", "--regalloc", "none"]
+    flags: ["--liveness", "trivial", "--regalloc", "none"]
   },
   {
     name: "greedy",
-    flags: ["-O1", "--liveness", "dataflow", "--regalloc", "greedy"]
+    flags: ["--liveness", "dataflow", "--regalloc", "greedy"]
   },
   {
     name: "better",
+    flags: ["--liveness", "dataflow", "--regalloc", "better"]
+  },
+  { 
+    name: "clang", 
+    flags: ["--clang"]
+  },
+  {
+    name: "baseline optimized",
+    flags: ["-O1", "--liveness", "trivial", "--regalloc", "none"]
+  },
+  {
+    name: "greedy optimized",
+    flags: ["-O1", "--liveness", "dataflow", "--regalloc", "greedy"]
+  },
+  {
+    name: "better optimized",
     flags: ["-O1", "--liveness", "dataflow", "--regalloc", "better"]
   },
-  { name: "clang", flags: ["-O1", "--clang"] }
+  { 
+    name: "clang optimized", 
+    flags: ["-O1", "--clang"]
+  }
 ];
 
 const REPETITIONS = 10;
